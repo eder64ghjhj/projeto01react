79738e0fcbf01a260f2a729a3d9ebf81945119ae
eder64ghjhj <166666766+eder64ghjhj@users.noreply.github.com>
2024-10-26 17:42:45 -0300

    Add files via upload

---
+++ projeto01/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import ListaDetarefas from './components/ListaDeTarefas'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="App">
+    <ListaDetarefas/>
+    </div>
+    </>
+  )
+}
+
+export default App
